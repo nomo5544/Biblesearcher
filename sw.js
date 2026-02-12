70fbde529334f02530d5896e769efb7ec4f522ee
Oleh <nomo5544@ukr.net>
2026-02-13 01:35:18 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'bible-searcher-v1';
+const ASSETS = [
+  'index.html',
+  'reader.html',
+  'reader.js',
+  'app.webmanifest',
+  'bibleTextUA.json',
+  'bibleTextRU.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+// Встановлення: зберігаємо всі файли в пам'ять пристрою
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Робота: якщо немає інтернету, беремо файли з пам'яті
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
