ad7151a890eccb6d25a85eb51af9540f3a6fa922
Oleh <nomo5544@ukr.net>
2026-02-22 00:38:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -319,7 +319,7 @@ const maps = {
 
     if (exactMatch) exactMatch.onchange = window.performSearch;
     if (fontSizeRange) {
-        const savedSize = localStorage.getItem('searchFontSize') || '21';
+        const savedSize = localStorage.getItem('searchFontSize') || '22';
         fontSizeRange.value = savedSize;
         resultsDiv.style.fontSize = savedSize + 'px';
         fontSizeRange.oninput = () => {
