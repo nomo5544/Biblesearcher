0c9626ffe3d21d1918082b1e48d638b2022b1c00
Oleh <nomo5544@ukr.net>
2026-02-24 00:03:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bible-searcher-v1.01221'; // Оновив версію тут
+const CACHE_NAME = 'bible-searcher-v1.01222'; // Оновив версію тут
 const ASSETS = [
   'index.html',
   'reader.html',
