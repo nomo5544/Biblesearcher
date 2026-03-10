fdc3bc12f3b8ae5c3d33d1daef305c73537aad22
Oleh <nomo5544@ukr.net>
2026-03-10 12:27:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -373,7 +373,7 @@ const maps = {
             if (!refs) return;
             navigator.clipboard.writeText(refs).then(() => {
                 const old = copyRefsBtn.innerText;    
-                copyRefsBtn.innerText = '✅';
+                copyRefsBtn.innerText = '☑';
                 setTimeout(() => copyRefsBtn.innerText = old, 2000);
             });
         };
